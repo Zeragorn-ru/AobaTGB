fffee4a0bed62649cedbecf1978ccfba1095bb88
Zeragorn <rosokin52@gmail.com>
2025-05-02 13:12:23 +0300

    0.0.3b
    added get_listdir method to RemoteFileDownloader class

--- mc_server_get_stats/sftp_get_file.py
+++ mc_server_get_stats/sftp_get_file.py
@@ -12,6 +12,7 @@ class RemoteFileDownloader:
         self.username = username
         self.password = password
         self.file = None
+        self.listdir = None
 
     def json_file_load(self, file_path:str):
         transport = paramiko.Transport((self.host, self.port))
@@ -35,3 +36,19 @@ class RemoteFileDownloader:
             raise ValueError("file don't downloaded")
         return self.file
 
+    def get_listdir(self, remote_path:str):
+        transport = paramiko.Transport((self.host, self.port))
+        transport.connect(username=self.username, password=self.password)
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+
+        try:
+            self.listdir = sftp.listdir(remote_path)
+
+        except Exception as e:
+            error(e)
+
+        sftp.close()
+        transport.close()
+
+        return self.listdir
