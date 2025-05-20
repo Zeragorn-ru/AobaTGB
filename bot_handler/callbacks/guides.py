574c06d4846ed533de7254a058d623220efc003d
Zeragorn <rosokin52@gmail.com>
2025-05-20 21:32:26 +0300

    1.2.2a
    first menu for guides

--- bot_handler/callbacks/guides.py
+++ bot_handler/callbacks/guides.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import asyncio
 from aiogram import Router, F
-from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile, InputMediaPhoto
 from aiogram.filters import Command
 
 from bot_handler.msg_content import Msg, SH, SFTP
@@ -9,7 +9,25 @@ from boot import debug, info, warn, error, critical
 
 router: Router = Router()
 bot_content = Msg()
+file = FSInputFile("./assets/guides_icon.png")
 
 @router.callback_query(F.data == "guides")
 async def guides(callback: CallbackQuery):
-    await callback.answer("test")
+    original_chat_id = callback.message.chat.id
+    original_message_id = callback.message.message_id
+    content = await bot_content.guides()
+
+    guides_content = InputMediaPhoto(
+        media=file,
+        caption=content["text"],
+        parse_mode="HTML"
+    )
+
+    await callback.bot.edit_message_media(
+        chat_id=original_chat_id,
+        message_id=original_message_id,
+        media=guides_content,
+        reply_markup=content["buttons"]
+    )
+
+    await callback.answer("")
--- bot_handler/comands/start.py
+++ bot_handler/comands/start.py
@@ -3,7 +3,7 @@ import sys
 
 import asyncio
 from aiogram import Bot, Router, types, F
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile, InputMediaPhoto
 from aiogram.filters import Command
 
 from bot_handler.msg_content import Msg
@@ -33,10 +33,15 @@ async def refresh_button(callback: CallbackQuery):
     original_message_id = callback.message.message_id
     original_chat_id = callback.message.chat.id
 
-    await callback.bot.edit_message_caption(
+    start_content = InputMediaPhoto(
+        media=file,
+        caption=start_msg_info["start_text"],
+        parse_mode="HTML"
+    )
+
+    await callback.bot.edit_message_media(
         chat_id=original_chat_id,
         message_id=original_message_id,
-        caption = start_msg_info["start_text"],
-        reply_markup=start_msg_info["buttons"],
-        parse_mode="HTML"
+        media=start_content,
+        reply_markup=start_msg_info["buttons"]
     )
